2a4cd8b3e821de1bc48b7f1f4f7178a04cfad504
Hyunjooleee <hjdjl7@naver.com>
2019-08-20 17:30:07 +0900

    third

--- src/main/java/com/example/test_springboot02/controller/CommonCodeController.java
+++ src/main/java/com/example/test_springboot02/controller/CommonCodeController.java
@@ -1,12 +1,29 @@
 package com.example.test_springboot02.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value = "/commoncode")
 public class CommonCodeController{
-    @RequestMapping(value = "/edit", method = { RequestMethod.GET, RequestMethod.POST})
-    public void edit(){}
+    @RequestMapping(value = "/commonCode/edit")
+    public void edit_action(){
+    }
+    @RequestMapping(value = "/commonCode/{action}")
+    public ModelAndView edit(@RequestParam Map<String, Object>paramMap, @PathVariable String action, ModelAndView modelandView){
+        String viewName = "/commonCode/";
+        Map<String,Object> resultMap = new HashMap<String,Object>();
+        if("read02".equals(action)){
+            viewName = viewName + action;
+            resultMap = paramMap;
+        }
+        modelandView.setViewName(viewName);
+        modelandView.addObject("resultMap",resultMap);
+        return modelandView;
+    }
 }
