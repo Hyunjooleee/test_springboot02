53203a410aa3e4ef9f1e498c8e3cfed55e766a20
Hyunjooleee <hjdjl7@naver.com>
2019-08-20 10:52:15 +0900

    first

---
+++ src/main/java/com/example/test_springboot02/controller/Try_HomeController.java
@@ -0,0 +1,12 @@
+package com.example.test_springboot02.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Try_HomeController{
+    @RequestMapping(value = "/try_home")
+    public String action(){
+        return "home";
+    }
+}
---
+++ src/main/java/com/example/test_springboot02/controller/Try_MenuController.java
@@ -0,0 +1,11 @@
+package com.example.test_springboot02.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Try_MenuController{
+    @RequestMapping(value = "/menu/login")
+    public void menuAction(){
+    }
+}
